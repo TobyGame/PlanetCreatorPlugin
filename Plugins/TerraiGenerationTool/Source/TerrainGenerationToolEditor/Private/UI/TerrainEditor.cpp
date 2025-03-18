e9863e6fb7a9f4742f8614ab8af72c1115de20aa
TobyGame <tobygame1234@gmail.com>
2025-03-18 20:36:08 +0100

    Add TerrainEditor core class to support terrain editing
    Introduce `FTerrainEditor` as the central class for the Terrain Generation Tool Editor. This class manages initialization, layout, tab registration, and editing functionality for terrain assets, establishing the foundation for the editor's workflow.

---
+++ Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Private/UI/TerrainEditor.cpp
@@ -0,0 +1,28 @@
+﻿#include "UI/TerrainEditor.h"
+#include "UI/TerrainEditor.h"
+#include "UI/TerrainEditorMode.h"
+#include "Modules/ModuleManager.h"
+#include "EditorStyleSet.h"
+
+#define LOCTEXT_NAMESPACE "TerrainEditor"
+
+void FTerrainEditor::InitTerrainEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UObject* TerrainAsset)
+{
+	EditingObject = TerrainAsset;
+
+	TerrainEditorMode = MakeShareable(new FTerrainEditorMode(SharedThis(this)));
+
+	InitAssetEditor(Mode, InitToolkitHost, FName("TerrainEditorApp"), TerrainEditorMode->GetLayout(), true, true, TerrainAsset);
+}
+
+void FTerrainEditor::RegisterTabSpawners(const TSharedRef<FTabManager>& TabManager)
+{
+	FWorkflowCentricApplication::RegisterTabSpawners(TabManager);
+}
+
+void FTerrainEditor::UnregisterTabSpawners(const TSharedRef<FTabManager>& TabManager)
+{
+	FWorkflowCentricApplication::UnregisterTabSpawners(TabManager);
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Public/UI/TerrainEditor.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "WorkflowOrientedApp/WorkflowCentricApplication.h"
+
+class FTerrainEditorMode;
+
+/**
+ * Core class for the Terrain Generator Tool Editor
+ */
+class FTerrainEditor : public FWorkflowCentricApplication
+{
+public:
+	void InitTerrainEditor(const EToolkitMode::Type Mode, const TSharedPtr<IToolkitHost>& InitToolkitHost, UObject* TerrainAsset);
+
+	virtual FName GetToolkitFName() const override { return FName("TerrainEditor"); }
+	virtual FText GetBaseToolkitName() const override { return FText::FromString("Terrain Editor"); }
+	virtual FString GetWorldCentricTabPrefix() const override { return TEXT("TerrainEditor"); }
+	UObject* GetEdittingObject() const { return EditingObject; }
+
+protected:
+	virtual void RegisterTabSpawners(const TSharedRef<FTabManager>& TabManager) override;
+	virtual void UnregisterTabSpawners(const TSharedRef<FTabManager>& TabManager) override;
+
+private:
+	UObject* EditingObject;
+	TSharedPtr<FTerrainEditorMode> TerrainEditorMode;
+};
