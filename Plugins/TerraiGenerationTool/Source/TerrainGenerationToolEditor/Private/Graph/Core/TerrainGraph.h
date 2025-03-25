6a8d1817b0b35e83028790068acdf73df4f89646
TobyGame <tobygame1234@gmail.com>
2025-03-25 20:31:13 +0100

    Add core classes for Terrain Graph and Schema
    Introduce `UTerrainGraph` and `UTerrainGraphSchema` to support terrain graph functionality. Implements node-based context menu actions and connection validation to facilitate graph editing. This sets up the foundation for terrain generation tool features.

---
+++ Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Private/Graph/Core/TerrainGraph.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EdGraph/EdGraph.h"
+#include "TerrainGraph.generated.h"
+
+UCLASS()
+class UTerrainGraph : public UEdGraph
+{
+	GENERATED_BODY()
+
+public:
+	UTerrainGraph();
+};
