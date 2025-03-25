b0ac846bd3ec59d8216e3032cb5f67a8305d08de
TobyGame <tobygame1234@gmail.com>
2025-03-25 22:41:12 +0100

    Add initial implementation of UTerrainGraphNode class
    Introduces `UTerrainGraphNode` with core functionalities for node title, category, and pin allocation. Defines supporting structures like `FTerrainPinDescriptor` and `ETerrainValueType` for terrain graph nodes, enabling flexible pin management. This serves as the foundation for the terrain generation tool's graph editor.

---
+++ Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Private/Graph/Core/TerrainGraphNode.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EdGraph/EdGraphNode.h"
+#include "TerrainGraphNode.generated.h"
+
+UENUM()
+enum class ETerrainValueType
+{
+	None,
+	Float,
+	HeightMap,
+	Mask,
+	Vector2D
+};
+
+USTRUCT()
+struct FTerrainPinDescriptor
+{
+	GENERATED_BODY()
+
+	UPROPERTY() FName PinName;
+	UPROPERTY() ETerrainValueType DataType = ETerrainValueType::None;
+	UPROPERTY() int32 Index = -1;
+	UPROPERTY() bool bIsInput = false;
+	UPROPERTY() bool bIsOptional = false;
+	UPROPERTY() bool bIsMainInput = false;
+	UPROPERTY() bool bIsPreviewable = true;
+};
+
+UCLASS()
+class UTerrainGraphNode : public UEdGraphNode
+{
+	GENERATED_BODY()
+
+public:
+	UTerrainGraphNode();
+
+	virtual void AllocateDefaultPins() override;
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+	virtual FString GetCategoryName() const;
+
+protected:
+	virtual void DefinePins(TArray<FTerrainPinDescriptor>& OutPins) {}
+
+public:
+	UPROPERTY() TArray<FTerrainPinDescriptor> PinDescriptions;
+	UPROPERTY() FText NodeDisplayName;
+	UPROPERTY() FString CategoryName;
+	UPROPERTY() bool bIsCached = false;
+};
