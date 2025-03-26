e17058e7160529693bb29ab4a6abe1e44f901ce0
TobyGame <tobygame1234@gmail.com>
2025-03-26 19:21:14 +0100

    Add TerrainNodeRegistry to manage terrain node registration
    Introduce `FTerrainNodeRegistry` for centralized management of terrain graph nodes. This includes a static registry and methods to register nodes with category, display name, and node class, as well as to retrieve the registry list.

---
+++ Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Public/Graph/Core/TerrainNodeRegistry.cpp
@@ -0,0 +1,13 @@
+﻿#include "Graph/Core/TerrainNodeRegistry.h"
+
+static TArray<FTerrainNodeRegistryEntry> Registry;
+
+void FTerrainNodeRegistry::Register(const FString& Category, const FString& DisplayName, TSubclassOf<UTerrainGraphNode> NodeClass)
+{
+	Registry.Add({Category, DisplayName, NodeClass});
+}
+
+const TArray<FTerrainNodeRegistryEntry>& FTerrainNodeRegistry::Get()
+{
+	return Registry;
+}
