d07aff87d8e4610045b0a3452802cdeed4801076
TobyGame <tobygame1234@gmail.com>
2025-03-27 19:51:15 +0100

    Remove Terrain Generation Tool plugin.
    Deleted the Terrain Generation Tool plugin, including all related source files, headers, and build definitions. This removes terrain generation editor features, asset tools, graph systems, and associated modules.

--- Plugins/TerraiGenerationTool/Source/TerraiGenerationTool/Private/Assets/TerrainAsset.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "Assets/TerrainAsset.h"
-
-UTerrainAsset::UTerrainAsset()
-{
-	// Default values
-	TerrainScale = 1.0f;
-}
--- Plugins/TerraiGenerationTool/Source/TerrainGenerationToolEditor/Private/UI/TerrainTabFactory.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "TerrainTabFactory.h"
-#include "TerrainEditor.h"
-
-FTerrainTabFactory::FTerrainTabFactory(FName InTabID, TSharedPtr<FTerrainEditor> InEditor)
-	: FWorkflowTabFactory(InTabID, InEditor), TerrainEditor(InEditor)
-{
-	TabLabel = FText::FromString(InTabID.ToString());
-	bIsSingleton = true;
-}
