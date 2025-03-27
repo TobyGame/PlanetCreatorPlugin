46fdd3d3b3fc0656d15ae20d83c73a562173d6e3
TobyGame <tobygame1234@gmail.com>
2025-03-27 19:54:11 +0100

    Add initial implementation of TerrainGenerationTool plugin
    This commit introduces the TerrainGenerationTool plugin with its core and editor modules, configuration files, and build settings. It includes the necessary module setup, module dependencies, and an example icon file. The plugin is ready for further development and integration into the project.

---
+++ Plugins/TerrainGenerationTool/Source/TerrainGenerationToolEditor/Public/TerrainGenerationToolEditor.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class IAssetTypeActions;
+
+class FTerrainGenerationToolEditorModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
